2b1ca4a5f1aed0265c307a20a168d009b213f26c
Scott Hutchins <mayosquirrel@gmail.com>
2021-12-06 09:40:50 -0500

    Fixed some bugs

--- todo.cpp
+++ todo.cpp
@@ -14,7 +14,7 @@
  */
 bool to_bool(std::string const &s)
 {
-  return s != "0";
+	return s != "0";
 }
 
 /**
@@ -23,21 +23,21 @@ bool to_bool(std::string const &s)
  */
 std::map<std::string, bool> loadData(const std::string &filename)
 {
-  std::map<std::string, bool> result;
-  std::string line, word;
-  bool checked;
-  std::ifstream input(filename);
-  while (std::getline(input, line))
-  {
-    std::istringstream iss(line);
-    std::getline(iss, word);
-    std::string s(1, word.back());
-    checked = to_bool(s);
-    std::cout << "Checked:" << checked << std::endl;
-    word = word.substr(0, word.length() - 2);
-    result[word] = checked;
-  }
-  return result;
+	std::map<std::string, bool> result;
+	std::string line, word;
+	bool checked;
+	std::ifstream input(filename);
+	while (std::getline(input, line))
+	{
+		std::istringstream iss(line);
+		std::getline(iss, word);
+		std::string s(1, word.back());
+		checked = to_bool(s);
+		std::cout << "Checked:" << checked << std::endl;
+		word = word.substr(0, word.length() - 2);
+		result[word] = checked;
+	}
+	return result;
 }
 
 /**
@@ -46,10 +46,10 @@ std::map<std::string, bool> loadData(const std::string &filename)
  */
 void printList(std::map<std::string, bool> &todo)
 {
-  for (auto &entry : todo)
-  {
-    std::cout << entry.first << ": Finished: " << entry.second << std::endl;
-  }
+	for (auto &entry : todo)
+	{
+		std::cout << entry.first << ": Finished: " << entry.second << std::endl;
+	}
 }
 
 /**
@@ -59,13 +59,13 @@ void printList(std::map<std::string, bool> &todo)
  */
 void saveChanges(std::map<std::string, bool> &todo, std::string fileName)
 {
-  std::ofstream out;
-  out.open(fileName);
-  for (auto &entry : todo)
-  {
-    out << entry.first << " " << entry.second << std::endl;
-  }
-  out.close();
+	std::ofstream out;
+	out.open(fileName);
+	for (auto &entry : todo)
+	{
+		out << entry.first << " " << entry.second << std::endl;
+	}
+	out.close();
 }
 
 /**
@@ -74,85 +74,85 @@ void saveChanges(std::map<std::string, bool> &todo, std::string fileName)
  */
 void main_loop(std::map<std::string, bool> &todo)
 {
-  std::string userInput = "";
-  std::cout << "Enter 'exit' to save and quit" << std::endl;
-  // If you remove this the program breaks and I dont know why lol
-  std::getline(std::cin, userInput);
+	std::string userInput = "";
+	std::cout << "Enter 'exit' to save and quit" << std::endl;
+	// If you remove this the program breaks and I dont know why lol
+	std::getline(std::cin, userInput);
 
-  while (userInput != "exit")
-  {
-    int pid = fork();
-    if (pid == 0)
-    {
-      execlp("clear", "clear", nullptr);
-    }
-    else
-    {
-      waitpid(pid, nullptr, 0);
-    }
-    printList(todo);
-    std::cout << ">";
-    std::getline(std::cin, userInput);
-    if (userInput == "exit")
-    {
-      break;
-    }
-    else
-    {
-      std::istringstream iss(userInput);
-      std::string command, word;
-      iss >> command;
-      std::getline(iss, word);
-      word = word.substr(1);
-      if (command == "add")
-      {
-        todo[word] = false;
-      }
-      else if (command == "finish" || command == "done")
-      {
-        todo[word] = true;
-      }
-      else if (command == "remove" || command == "del")
-      {
-        if (todo.find(word) != std::end(todo))
-        {
-          todo.erase(word);
-        }
-      }
-      else
-      {
-        std::cout << "Enter a valid command" << std::endl;
-      }
-    }
-  }
+	while (userInput != "exit")
+	{
+		int pid = fork();
+		if (pid == 0)
+		{
+			execlp("clear", "clear", 0);
+		}
+		else
+		{
+			waitpid(pid, nullptr, 0);
+		}
+		printList(todo);
+		std::cout << ">";
+		std::getline(std::cin, userInput);
+		if (userInput == "exit")
+		{
+			break;
+		}
+		else
+		{
+			std::istringstream iss(userInput);
+			std::string command, word;
+			iss >> command;
+			std::getline(iss, word);
+			word = word.substr(1);
+			if (command == "add")
+			{
+				todo[word] = false;
+			}
+			else if (command == "finish" || command == "done")
+			{
+				todo[word] = true;
+			}
+			else if (command == "remove" || command == "del")
+			{
+				if (todo.find(word) != std::end(todo))
+				{
+					todo.erase(word);
+				}
+			}
+			else
+			{
+				std::cout << "Enter a valid command" << std::endl;
+			}
+		}
+	}
 }
 
 /**
- * @brief 
- * 
- * @return int 
+ * @brief
+ *
+ * @return int
  */
 int main()
 {
-  std::ifstream input_file;
-  std::string fileName;
-  std::cout << "Enter file name to open (if the file does not exsit a new one will be created)" << std::endl;
-  std::cin >> fileName;
-  fileName = fileName + ".txt";
-  input_file.open(fileName);
-  if (input_file)
-  {
-    std::map<std::string, bool> list = loadData(fileName);
-    main_loop(list);
-    saveChanges(list, fileName);
-  }
-  else
-  {
-    std::ofstream output(fileName);
-    output.close();
-    std::map<std::string, bool> list;
-    main_loop(list);
-    saveChanges(list, fileName);
-  }
-  return 0;
+	std::ifstream input_file;
+	std::string fileName;
+	std::cout << "Enter file name to open (if the file does not exsit a new one will be created)" << std::endl;
+	std::cin >> fileName;
+	fileName = fileName + ".txt";
+	input_file.open(fileName);
+	if (input_file)
+	{
+		std::map<std::string, bool> list = loadData(fileName);
+		main_loop(list);
+		saveChanges(list, fileName);
+	}
+	else
+	{
+		std::ofstream output(fileName);
+		output.close();
+		std::map<std::string, bool> list;
+		main_loop(list);
+		saveChanges(list, fileName);
+	}
+	return 0;
 }
